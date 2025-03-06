ac4b95aff888ca54c6278ec26fafa8f1fce5afdf
Sandhiya1321 <sandhiyasaprkle1234@gmail.com>
2025-03-06 12:29:36 +0530

    leetCode

---
+++ src/partitionArray.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+
+class partitionArray {
+    public int[] pivotArray(int[] nums, int pivot) {
+
+        int n=nums.length;
+        int[] res=new int[n];
+        int index=0;
+        int greterIndex=nums.length-1;
+        int pivotCount=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]<pivot){
+                res[index++]=nums[i];
+            }
+            else if(nums[i]==pivot)
+                pivotCount++;
+
+        }
+        while(pivotCount>0){
+            res[index++]=pivot;
+            pivotCount--;
+        }
+        for(int i=0;i<n;i++){
+            if(nums[i]>pivot){
+                res[index++]=nums[i];
+
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        partitionArray part=new partitionArray();
+        int[]  nums = {9,12,5,10,14,3,10};
+        int pivot = 10;
+        System.out.println(Arrays.toString(part.pivotArray(nums,pivot)));
+    }
+}
